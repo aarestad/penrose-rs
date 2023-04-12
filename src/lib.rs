a47ba4bf34f662c2fb3dff429b36520a1683a582
Peter Aarestad <aarestad@gmail.com>
2023-04-12 16:34:39 -0500

    serde!

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 #![feature(const_fn_floating_point_arithmetic)]
 
 use std::f64::consts::PI;
+use serde::{Serialize, Deserialize};
 use wasm_bindgen::prelude::*;
 
 #[allow(unused_imports)]
 use web_sys::console;
 
+#[derive(Serialize, Deserialize)]
 enum RobinsonTriangleType {
     ThinLeft,
     ThinRight,
@@ -14,6 +16,7 @@ enum RobinsonTriangleType {
 }
 
 
+#[derive(Serialize, Deserialize)]
 #[wasm_bindgen]
 pub struct RobinsonTriangle {
     triangle_type: RobinsonTriangleType,
@@ -48,7 +51,34 @@ impl RobinsonTriangle {
         }
     }
 
-    fn decompose(&self) -> [RobinsonTriangle; 2] {
+    fn base_points(&self) -> [(f64, f64); 2] {
+        let mut theta_one = self.rotation + self.vertex_angle() / 2.0;
+
+        if theta_one >= 2.0 * PI {
+            theta_one -= 2.0 * PI;
+        }
+
+        let mut theta_two = self.rotation - self.vertex_angle() / 2.0;
+
+        if theta_two < 0.0 {
+            theta_two += 2.0 * PI;
+        }
+
+        // reflect Ys across x=apex.x since positive Y is down in graphics coordinates
+        let base_point_1 = (
+            self.apex.0 + self.leg_length * theta_one.cos(),
+            self.apex.1 - self.leg_length * theta_one.sin(),
+        );
+
+        let base_point_2 = (
+            self.apex.0 + self.leg_length * theta_two.cos(),
+            self.apex.1 - self.leg_length * theta_two.sin(),
+        );
+
+        [base_point_1, base_point_2]
+    }
+
+    pub fn decompose(&self) -> JsValue {
         let new_point: (f64, f64) = match self.triangle_type {
             RobinsonTriangleType::ThinLeft => todo!(),
             RobinsonTriangleType::ThinRight => todo!(),
@@ -112,38 +142,6 @@ impl RobinsonTriangle {
             }
         };
 
-        [triangle_one, triangle_two]
-    }
-
-    fn base_points(&self) -> [(f64, f64); 2] {
-        let mut theta_one = self.rotation + self.vertex_angle() / 2.0;
-
-        if theta_one >= 2.0 * PI {
-            theta_one -= 2.0 * PI;
-        }
-
-        let mut theta_two = self.rotation - self.vertex_angle() / 2.0;
-
-        if theta_two < 0.0 {
-            theta_two += 2.0 * PI;
-        }
-
-        // reflect Ys across x=apex.x since positive Y is down in graphics coordinates
-        let base_point_1 = (
-            self.apex.0 + self.leg_length * theta_one.cos(),
-            self.apex.1 - self.leg_length * theta_one.sin(),
-        );
-
-        let base_point_2 = (
-            self.apex.0 + self.leg_length * theta_two.cos(),
-            self.apex.1 - self.leg_length * theta_two.sin(),
-        );
-
-        [base_point_1, base_point_2]
-    }
-
-    pub fn points(&self) -> Box<[f64]> {
-        let base_points = self.base_points();
-        Box::from([self.apex.0, self.apex.1, base_points[0].0, base_points[0].1, base_points[1].0, base_points[1].1])
+        serde_wasm_bindgen::to_value(&[triangle_one, triangle_two]).unwrap()
     }
 }
